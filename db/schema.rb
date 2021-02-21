d6904ae41ff3dd3d047e07cb7fffbeed429e3909
Cleyton Silva <silva.rcleyton@gmail.com>
2021-02-21 00:06:53 -0300

    Create product

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_222544) do
+ActiveRecord::Schema.define(version: 2021_02_20_224057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "value"
+    t.string "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
